741163014d6d37e21d8501212d29e81856822543
Adrian Medina <adrian.medina@mail.yu.edu>
2015-01-28 18:46:49 -0500

    extend seq for sequences, simple-strings, and hash-tables

--- src/transducers.lisp
+++ src/transducers.lisp
@@ -7,8 +7,18 @@
   (declare (optimize speed (safety 0) (debug 0) (space 0)))
   o)
 
-(define-compiler-macro seq (&whole form o)
-  (if (constantp o)
-      (funcall #'seq o)
-      form))
+(defmethod seq ((s simple-string))
+  (declare (optimize speed (safety 0) (debug 0) (space 0)))
+  (loop
+    for ch across s collect ch))
+
+(defmethod seq ((o hash-table))
+  (declare (optimize speed (safety 0) (debug 0) (space 0)))
+  (loop
+    for k being the hash-keys in o
+    for v being the hash-values in o
+    collect (cons k v)))
+
+
+
 
