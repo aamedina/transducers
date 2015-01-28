4599d1b7946689ddf15e7cdf14440d9cfcd91acb
Adrian Medina <adrian.medina@mail.yu.edu>
2015-01-28 17:47:06 -0500

    define seq as a generic method

--- src/package.lisp
+++ src/package.lisp
@@ -1,3 +1,5 @@
 (defpackage :transducers
   (:use :common-lisp))
 
+
+
--- src/transducers.lisp
+++ src/transducers.lisp
@@ -1,1 +1,14 @@
 (in-package :transducers)
+
+(defgeneric seq (o)
+  (declare (optimize speed (space 0))))
+
+(defmethod seq ((o sequence))
+  (declare (optimize speed (safety 0) (debug 0) (space 0)))
+  o)
+
+(define-compiler-macro seq (&whole form o)
+  (if (constantp o)
+      (funcall #'seq o)
+      form))
+
