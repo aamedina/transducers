d8100c576cac4206a7272ca2b37ae34ad8ba3956
Adrian Medina <adrian.medina@mail.yu.edu>
2015-01-28 19:46:32 -0500

    redefine reduce in terms of extensible coll-reduce

--- src/package.lisp
+++ src/package.lisp
@@ -1,5 +1,7 @@
 (defpackage :transducers
-  (:use :common-lisp))
+  (:use :common-lisp)
+  (:shadow :map :reduce))
 
-
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (proclaim '(optimize speed)))
 
--- src/transducers.lisp
+++ src/transducers.lisp
@@ -20,8 +20,32 @@
       for v being the hash-values in hash-table
       collect (cons k v))))
 
+(defgeneric coll-reduce (coll f val)
+  (declare (optimize speed (space 0))))
+
+(defmethod coll-reduce ((coll null) f val)
+  (declare (function f) (optimize speed (safety 0) (debug 0) (space 0)))
+  (or val (funcall f)))
+
+(defmethod coll-reduce ((coll sequence) f val)
+  (declare (function f) (optimize speed (safety 0) (debug 0) (space 0)))
+  (if val
+      (cl:reduce f coll :initial-value val)
+      (cl:reduce f coll)))
+
+(define-compiler-macro reduce (f coll &optional (val nil providedp))
+  (if providedp
+      `(coll-reduce ,val ,f ,coll)
+      `(coll-reduce ,coll ,f nil)))
+
+(defun reduce (f coll-or-init &optional (init-or-coll nil providedp))
+  (declare (function f) (optimize speed (safety 0) (debug 0) (space 0)))
+  (if providedp
+      (coll-reduce init-or-coll f coll-or-init)
+      (coll-reduce coll-or-init f init-or-coll)))
+
 (defun comp (&optional f g &rest more)
-  (cond (more (comp (comp f g) more))
+  (cond (more (reduce #'comp (comp f g) more))
         (g (lambda (&rest args) (funcall f (apply g args))))
         (f f)
         (t #'identity)))
