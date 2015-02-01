fd1b87c11bfa6bd27aa039b78fe8c97374eb8063
Adrian Medina <adrian.medina@mail.yu.edu>
2015-01-31 21:41:39 -0500

    assoc-in!

--- src/vector.lisp
+++ src/vector.lisp
@@ -13,18 +13,35 @@
 
 (define-constant empty-vector (make-instance 'persistent-vector))
 
+(declaim (inline tailoff))
 (defun tailoff (vec)
-  (declare (or persistent-vector transient-vector))
-  (let ((cnt (:count vec)))
+  (declare ((or persistent-vector transient-vector) vec))
+  (let ((cnt (length vec)))
+    (declare (fixnum cnt))
     (the fixnum (if (< cnt 32)
                     0
-                    (dec cnt)))))
+                    (bit-shift-left (bit-shift-right (dec cnt) 5) 5)))))
+
+(declaim (inline array-for))
+(defun array-for (vec i)
+  (declare ((or persistent-vector transient-vector) vec) ((integer 0 *) i))
+  (when (and (>= i 0) (< i (:count vec)))
+    (when (>= i (tailoff vec))
+      (return-from array-for (:tail vec)))
+    (let ((node (:root vec)))
+      (loop
+        for level from (:shift vec) downto 0 by 5
+        do (->> (bit-and (bit-shift-right i level) #x01f)
+                (aref (cdr node))
+                (setf node)))
+      (the array (cdr node)))))
 
 (defmethod sequence:length ((o persistent-vector))
   (:count o))
 
 (defmethod sequence:elt ((o persistent-vector) index)
-  (declare (ignore o index)))
+  (when-let (node (array-for o index))
+    (aref node (bit-and index #x01f))))
 
 (defmethod (setf sequence:elt) (new-value (o persistent-vector) index)
   (error "Cannot mutate persistent data structures"))
@@ -53,10 +70,28 @@
   (:count o))
 
 (defmethod sequence:elt ((o transient-vector) index)
-  (declare (ignore o index)))
+  (when-let (node (array-for o index))
+    (aref node (bit-and index #x01f))))
+
+(defun assoc-in! (level node i val)
+  (if (zerop shift)
+      (progn
+        (setf (aref (cdr node) (bit-and i #x01f)) val)
+        node)
+      (let ((subidx (bit-and (bit-shift-right i level) #x01f)))
+        (assoc-in! (- level 5) (aref (cdr node) subidx) i val))))
 
 (defmethod (setf sequence:elt) (new-value (o transient-vector) index)
-  (error "Cannot mutate transient data structures"))
+  (cond ((and (>= index 0) (< index (:count o)))
+         (if (>= index (tailoff o))
+             (progn
+               (aref (:tail o) (bit-and index #x01f))
+               o)
+             (progn
+               (setf (:root o) (assoc-in! shift root index new-value))
+               o)))
+        ((= index (:count o))
+         (conj! o new-value))))
 
 (defmethod sequence:adjust-sequence ((o transient-vector) length
                                      &key initial-element initial-contents)
