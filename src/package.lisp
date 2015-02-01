8518259b43d3c54dee32ba4978fe3c483474414d
Adrian Medina <adrian.medina@mail.yu.edu>
2015-02-01 14:28:11 -0500

    implement print-vector

--- src/package.lisp
+++ src/package.lisp
@@ -1,6 +1,6 @@
 (defpackage :transducers
   (:use :common-lisp :alexandria :sb-ext :sb-thread)
-  (:shadow :define-constant :map :remove :replace :first :rest
+  (:shadow :define-constant :map :remove :replace :first :rest :pop
            :bit-not :bit-and :bit-xor))
 
 (declaim (optimize speed))
--- src/vector.lisp
+++ src/vector.lisp
@@ -14,7 +14,7 @@
 (declaim (inline tailoff))
 (defun tailoff (vec)
   (declare ((or persistent-vector transient-vector) vec))
-  (let ((cnt (length vec)))
+  (let ((cnt (:count vec)))
     (declare (fixnum cnt))
     (the fixnum (if (< cnt 32)
                     0
@@ -70,9 +70,13 @@
   (when-let (node (array-for o index))
     (aref node (bit-and index #x01f))))
 
-(defun ensure-editable (vec)
-  (when (null (car (:root vec)))
-    (error "Transient used after persistent! call")))
+(defun ensure-editable (vec &optional (node nil nodep))
+  (if nodep
+      (if (eq (car node) (car (:root vec)))
+          node
+          (cons (car (:root vec)) (copy-array (cdr node))))
+      (when (null (car (:root vec)))
+        (error "Transient used after persistent! call"))))
 
 (defun assoc-in! (level node i val)
   (if (zerop level)
@@ -82,14 +86,54 @@
       (let ((subidx (bit-and (bit-shift-right i level) #x01f)))
         (assoc-in! (- level 5) (aref (cdr node) subidx) i val))))
 
+(defun push-tail (vec level parent tailnode)
+  (setf parent (ensure-editable parent))
+  (let* ((subidx (-> (bit-shift-right (dec (:count vec)) level)
+                     (bit-and #x01f)))
+         (ret parent)
+         (node-to-insert (if (= level 5)
+                             tailnode
+                             (if-let (child (aref (cdr parent) subidx))
+                               (push-tail vec (- level 5) child tailnode)
+                               (new-path (car (:root vec)) (- level 5)
+                                         tailnode)))))
+    (setf (aref (cdr ret) subidx) node-to-insert)
+    ret))
+
+(defun new-path (edit level node)
+  (if (zerop level)
+      node
+      (let ((ret (cons edit (make-array 32))))
+        (setf (aref (cdr ret) 0) (new-path edit (- level 5) node))
+        ret)))
+
 (declaim (inline vec-conj!))
 (defun vec-conj! (tcoll val)
   (declare (transient-vector tcoll))
   (let ((i (:count tcoll)))
-    (when (< (- i (tailoff tcoll)) 32)
-      (setf (aref (:tail tcoll) (bit-and i #x01f)) val)
-      (setf (:count tcoll) (1+ i))
-      (return-from vec-conj! tcoll))))
+    (if (< (- i (tailoff tcoll)) 32)
+        (progn
+          (setf (aref (:tail tcoll) (bit-and i #x01f)) val)
+          (setf (:count tcoll) (inc i)))
+        (let ((newroot nil)
+              (tailnode (cons (car (:root tcoll)) (:tail tcoll)))
+              (tail (setf (:tail tcoll) (make-array 32)))
+              (newshift (:shift tcoll)))
+          (setf (aref tail 0) val)
+          (if (> (bit-shift-right (:count tcoll) 5) (bit-shift-left 1 newshift))
+              (progn
+                (setf newroot (cons (car (:root tcoll)) (make-array 32)))
+                (setf (aref (cdr newroot) 0) (:root tcoll))
+                (setf (aref (cdr newroot) 1) (new-path (car (:root tcoll))
+                                                       (:shift tcoll)
+                                                       tailnode))
+                (setf newshift (+ newshift 5)))
+              (setf newroot (push-tail tcoll (:shift tcoll) (:root tcoll)
+                                       tailnode)))
+          (setf (:root tcoll) newroot)
+          (setf (:shift tcoll) newshift)
+          (setf (:count tcoll) (inc i))))
+    (the transient-vector tcoll)))
 
 (declaim (inline conj!))
 (defun conj! (tcoll val)
@@ -120,6 +164,58 @@
 (defmethod sequence:make-sequence-iterator ((o transient-vector)
                                             &key from-end start end)
   (declare (ignore o from-end start end)))
+
+(defun pop-tail (tcoll level node)
+  (setf node (ensure-editable node))
+  (let ((subidx (bit-and (bit-shift-right (- (:count tcoll) 2) level) #x01f)))
+    (cond ((> level 5) (let ((newchild (pop-tail tcoll (- level 5)
+                                                 (aref (cdr node) subidx))))
+                         (if (and (null newchild) (zerop subidx))
+                             nil
+                             (progn
+                               (setf (aref (cdr node) subidx) newchild)
+                               node))))
+          ((= subidx 0) nil)
+          (t (setf (aref (cdr node) subidx) nil)
+             node))))
+
+(defun editable-array-for (tcoll i)
+  (if (and (>= i 0) (< i (:count tcoll)))
+      (if (>= i (tailoff tcoll))
+          (:tail tcoll)
+          (let ((node (:root tcoll)))
+            (loop
+              for level from (:shift tcoll) downto 0 by 5
+              do (->> (bit-and (bit-shift-right i level) #x01f)
+                      (aref (cdr node))
+                      (ensure-editable tcoll)
+                      (setf node)))
+            (cdr node)))
+      (error "Index out of bounds")))
+
+(defun pop! (tcoll)
+  (ensure-editable tcoll)
+  (let ((cnt (:count tcoll)))
+    (cond ((zerop cnt) (error "Can't pop empty vector"))
+          
+          ((= cnt 1) (setf (:count tcoll) 0) tcoll)
+
+          ((plusp (bit-and (dec cnt) #x01f))
+           (setf (:count tcoll) (dec cnt)) tcoll)
+
+          (t (let* ((newtail (editable-array-for tcoll (- cnt 2)))
+                    (newroot (pop-tail tcoll (:shift tcoll) (:root tcoll)))
+                    (newshift (:shift tcoll)))
+               (when (null newroot)
+                 (setf newroot (cons (car (:root tcoll)) (make-array 32))))
+               (when (and (> (:shift tcoll) 5) (null (aref (cdr newroot) 1)))
+                 (setf newroot (ensure-editable tcoll (aref (cdr newroot) 0)))
+                 (setf newshift (- newshift 5)))
+               (setf (:root tcoll) newroot)
+               (setf (:shift tcoll) newshift)
+               (setf (:count tcoll) (dec cnt))
+               (setf (:tail tcoll) newtail)
+               tcoll)))))
 
 (defun editable-root (node)
   (cons *current-thread* (copy-array (cdr node))))
@@ -155,8 +251,12 @@
     (t tcoll)))
 
 (defun print-vector (vec stream)
-  (declare (ignore vec))
   (princ "[" stream)
+  (dotimes (i (dec (length vec)))
+    (prin1 (elt vec i) stream)
+    (princ #\space stream))
+  (when (plusp (length vec))
+    (prin1 (elt vec (dec (length vec))) stream))
   (princ "]" stream))
 
 (defmethod print-object ((object persistent-vector) stream)
